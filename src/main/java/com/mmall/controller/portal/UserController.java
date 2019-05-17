cc8a6aac64b0fe4013287a197c1436b76f9c7f73
tangpeng <tangpeng@ccclubs.com>
2019-05-17 20:31:14 +0800

    配置spring

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,12 @@
+package com.mmall.controller.portal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+//    public Object login(String username, String password, HttpSession session) {
+//
+//    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,7 @@
 <html>
+<head>
+    <meta charset="utf-8">
+</head>
 <body>
 <h2>第一次启动Tomcat 哈哈</h2>
 </body>
